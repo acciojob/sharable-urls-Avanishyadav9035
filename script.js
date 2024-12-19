fae8d100d3eef72208d94db2cdbab6ef29112d69
Avanishyadav9035 <132216852+Avanishyadav9035@users.noreply.github.com>
2024-12-20 00:40:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // your code here
-const nameInput = document.getElementById("name");
-const yearInput = document.getElementById("year");
-const submitButton = document.getElementById("button");
-const urlDisplay = document.getElementById("url");
+const nameInput = document.getElementById('name');
+const yearInput = document.getElementById('year');
+const submitButton = document.getElementById('button');
+const urlDisplay = document.getElementById('url');
 
-submitButton.addEventListner('click',()=<{
-	const name = encodeURIComponent.nameInput.value.trim();
-	const year = encodeURIComponent.yearInput.value.trim();
+submitButton.addEventListener('click', () => {
+      const name = encodeURIComponent(nameInput.value.trim());
+      const year = encodeURIComponent(yearInput.value.trim());
+
 
 let baseUrl = "https://localhost:8080/";
-let queryString = [];
-if (name) queryString.push(`name=${name}`)
-if (year) queryString.push(`year=${year}`)
-const updatedUrl = queryString.length ? `${baseUrl}?${queryString.join('&')}' : baseUrl;
- urlDisplay.textContent = updatedUrl;
+      let queryString = [];
+      if (name) queryString.push(`name=${name}`);
+      if (year) queryString.push(`year=${year}`);
+      const updatedUrl = queryString.length ? `${baseUrl}?${queryString.join('&')}` : baseUrl;
 
-})
-
+urlDisplay.textContent = updatedUrl;
+    });
